bda9b018ff09c56408bc13f767884cae11c50a4a
pofateeva <fateeva_pv@bk.ru>
2025-01-09 13:54:27 +0300

    Создание файла с клавиатурами (клавиатура городов)

---
+++ keyboards.py
@@ -0,0 +1,8 @@
+from telebot import types
+
+# Создание клваиатуры для выбора городов
+CITIES = ["Москва", "Санкт-Петербург", "Владивосток", "Казань", "Краснодар"]
+city_keyboard = types.ReplyKeyboardMarkup(row_width=3)
+
+city_buttons = [types.KeyboardButton(city) for city in CITIES]
+city_keyboard.add(*city_buttons)
