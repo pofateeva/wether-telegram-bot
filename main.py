38285d1fd85d3171ebe5f105b7e08d35d83d01c0
Aaron <owo.fox.uwu@gmail.com>
2025-01-09 13:48:19 +0300

    Добавила запуск бота

--- main.py
+++ main.py
@@ -6,3 +6,7 @@ bot = telebot.TeleBot(TELEGRAM_API_TOKEN)
 @bot.message_handler(commands=['start'])
 def start(message):
     bot.send_message(message.chat.id, "Привет! Я бот для вывода погоды!")
+
+if __name__ == "__main__":
+    print("Бот запущен!")
+    bot.polling(none_stop=True)
