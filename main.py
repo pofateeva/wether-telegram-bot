8e9c088b4786c7336c38cf6ad7f91db5ecb0392a
Aaron <owo.fox.uwu@gmail.com>
2025-01-09 16:01:15 +0300

    Добавила обработку кнопок

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import telebot
+import keyboards
 from config import TELEGRAM_API_TOKEN
 
 bot = telebot.TeleBot(TELEGRAM_API_TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, "Привет! Я бот для вывода погоды!")
+    bot.send_message(message.chat.id, "Привет! Выбери город, погоду которого ты хочешь узнать...", reply_markup = keyboards.city_keyboard)
+
+@bot.message_handler(func=lambda message: message.text)
+def get_city_weather(message):
+    if message.text in keyboards.CITIES:
+        city = message.text.strip()
+        bot.send_message(message.chat.id, f"Вы выбрали город: {city}")
+    else:
+        bot.send_message(message.chat.id, f"Упс, похоже такого города нет в списке!")
 
 if __name__ == "__main__":
     print("Бот запущен!")
-    bot.polling(none_stop=True)
+    bot.polling(none_stop=False)
